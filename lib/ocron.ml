b189f0bb2f26422c65ae027ba7a8cce323a645f7
Tejas Sanap <email@whereistejas.xyz>
2025-10-02 13:37:56 +0100

    Use `Core` types.

--- lib/ocron.ml
+++ lib/ocron.ml
@@ -1,416 +1,166 @@
-open Base
+open Core
 open Sexplib.Std
+module Time = Time_float_unix
+module Span = Time.Span
+module Zone = Time.Zone
 
-(*
-In the POSIX locale, the user or application shall ensure that a crontab
-entry is a text file consisting of lines of six fields each. The fields
-shall be separated by <blank>s. The first five fields shall be integer
-patterns that specify the following:
-1. Minute [0,59]
-2. Hour [0,23]
-3. Day of the month [1,31]
-4. Month of the year [1,12]
-5. Day of the week ([0,6] with 0=Sunday)
-*)
-module Time = struct
-  module Minute = struct
-    type t =
-      | Zero
-      | One
-      | Two
-      | Three
-      | Four
-      | Five
-      | Six
-      | Seven
-      | Eight
-      | Nine
-      | Ten
-      | Eleven
-      | Twelve
-      | Thirteen
-      | Fourteen
-      | Fifteen
-      | Sixteen
-      | Seventeen
-      | Eighteen
-      | Nineteen
-      | Twenty
-      | TwentyOne
-      | TwentyTwo
-      | TwentyThree
-      | TwentyFour
-      | TwentyFive
-      | TwentySix
-      | TwentySeven
-      | TwentyEight
-      | TwentyNine
-      | Thirty
-      | ThirtyOne
-      | ThirtyTwo
-      | ThirtyThree
-      | ThirtyFour
-      | ThirtyFive
-      | ThirtySix
-      | ThirtySeven
-      | ThirtyEight
-      | ThirtyNine
-      | Forty
-      | FortyOne
-      | FortyTwo
-      | FortyThree
-      | FortyFour
-      | FortyFive
-      | FortySix
-      | FortySeven
-      | FortyEight
-      | FortyNine
-      | Fifty
-      | FiftyOne
-      | FiftyTwo
-      | FiftyThree
-      | FiftyFour
-      | FiftyFive
-      | FiftySix
-      | FiftySeven
-      | FiftyEight
-      | FiftyNine
-    [@@deriving sexp, compare, show]
+module U = struct
+  let truncate_hr (time : Time.t) : Time.t =
+    let zone = Lazy.force Zone.local in
+    let date = Time.to_date ~zone time in
+    let ofday = Time.to_ofday ~zone time in
+    let parts = Time.Ofday.to_parts ofday in
+    let ofday = Time.Ofday.create ~hr:parts.hr () in
+    Time.of_date_ofday ~zone date ofday
 
-    let parse minute =
-      match minute with
-      | "0" -> Zero
-      | "1" -> One
-      | "2" -> Two
-      | "3" -> Three
-      | "4" -> Four
-      | "5" -> Five
-      | "6" -> Six
-      | "7" -> Seven
-      | "8" -> Eight
-      | "9" -> Nine
-      | "10" -> Ten
-      | "11" -> Eleven
-      | "12" -> Twelve
-      | "13" -> Thirteen
-      | "14" -> Fourteen
-      | "15" -> Fifteen
-      | "16" -> Sixteen
-      | "17" -> Seventeen
-      | "18" -> Eighteen
-      | "19" -> Nineteen
-      | "20" -> Twenty
-      | "21" -> TwentyOne
-      | "22" -> TwentyTwo
-      | "23" -> TwentyThree
-      | "24" -> TwentyFour
-      | "25" -> TwentyFive
-      | "26" -> TwentySix
-      | "27" -> TwentySeven
-      | "28" -> TwentyEight
-      | "29" -> TwentyNine
-      | "30" -> Thirty
-      | "31" -> ThirtyOne
-      | "32" -> ThirtyTwo
-      | "33" -> ThirtyThree
-      | "34" -> ThirtyFour
-      | "35" -> ThirtyFive
-      | "36" -> ThirtySix
-      | "37" -> ThirtySeven
-      | "38" -> ThirtyEight
-      | "39" -> ThirtyNine
-      | "40" -> Forty
-      | "41" -> FortyOne
-      | "42" -> FortyTwo
-      | "43" -> FortyThree
-      | "44" -> FortyFour
-      | "45" -> FortyFive
-      | "46" -> FortySix
-      | "47" -> FortySeven
-      | "48" -> FortyEight
-      | "49" -> FortyNine
-      | "50" -> Fifty
-      | "51" -> FiftyOne
-      | "52" -> FiftyTwo
-      | "53" -> FiftyThree
-      | "54" -> FiftyFour
-      | "55" -> FiftyFive
-      | "56" -> FiftySix
-      | "57" -> FiftySeven
-      | "58" -> FiftyEight
-      | "59" -> FiftyNine
-      | _ -> raise (Invalid_argument ("Invalid minute value: " ^ minute))
-  end
-
-  module Hour = struct
-    type t =
-      | Zero
-      | One
-      | Two
-      | Three
-      | Four
-      | Five
-      | Six
-      | Seven
-      | Eight
-      | Nine
-      | Ten
-      | Eleven
-      | Twelve
-      | Thirteen
-      | Fourteen
-      | Fifteen
-      | Sixteen
-      | Seventeen
-      | Eighteen
-      | Nineteen
-      | Twenty
-      | TwentyOne
-      | TwentyTwo
-      | TwentyThree
-    [@@deriving sexp, compare, show]
-
-    let parse hour =
-      match hour with
-      | "0" -> Zero
-      | "1" -> One
-      | "2" -> Two
-      | "3" -> Three
-      | "4" -> Four
-      | "5" -> Five
-      | "6" -> Six
-      | "7" -> Seven
-      | "8" -> Eight
-      | "9" -> Nine
-      | "10" -> Ten
-      | "11" -> Eleven
-      | "12" -> Twelve
-      | "13" -> Thirteen
-      | "14" -> Fourteen
-      | "15" -> Fifteen
-      | "16" -> Sixteen
-      | "17" -> Seventeen
-      | "18" -> Eighteen
-      | "19" -> Nineteen
-      | "20" -> Twenty
-      | "21" -> TwentyOne
-      | "22" -> TwentyTwo
-      | "23" -> TwentyThree
-      | _ -> raise (Invalid_argument ("Invalid hour value: " ^ hour))
-  end
-
-  module DayOfTheMonth = struct
-    type t =
-      | One
-      | Two
-      | Three
-      | Four
-      | Five
-      | Six
-      | Seven
-      | Eight
-      | Nine
-      | Ten
-      | Eleven
-      | Twelve
-      | Thirteen
-      | Fourteen
-      | Fifteen
-      | Sixteen
-      | Seventeen
-      | Eighteen
-      | Nineteen
-      | Twenty
-      | TwentyOne
-      | TwentyTwo
-      | TwentyThree
-      | TwentyFour
-      | TwentyFive
-      | TwentySix
-      | TwentySeven
-      | TwentyEight
-      | TwentyNine
-      | Thirty
-      | ThirtyOne
-    [@@deriving sexp, compare, show]
-
-    let parse day =
-      match day with
-      | "1" -> One
-      | "2" -> Two
-      | "3" -> Three
-      | "4" -> Four
-      | "5" -> Five
-      | "6" -> Six
-      | "7" -> Seven
-      | "8" -> Eight
-      | "9" -> Nine
-      | "10" -> Ten
-      | "11" -> Eleven
-      | "12" -> Twelve
-      | "13" -> Thirteen
-      | "14" -> Fourteen
-      | "15" -> Fifteen
-      | "16" -> Sixteen
-      | "17" -> Seventeen
-      | "18" -> Eighteen
-      | "19" -> Nineteen
-      | "20" -> Twenty
-      | "21" -> TwentyOne
-      | "22" -> TwentyTwo
-      | "23" -> TwentyThree
-      | "24" -> TwentyFour
-      | "25" -> TwentyFive
-      | "26" -> TwentySix
-      | "27" -> TwentySeven
-      | "28" -> TwentyEight
-      | "29" -> TwentyNine
-      | "30" -> Thirty
-      | "31" -> ThirtyOne
-      | _ -> raise (Invalid_argument ("Invalid day value: " ^ day))
-  end
-
-  module MonthOfTheYear = struct
-    type t =
-      | January
-      | February
-      | March
-      | April
-      | May
-      | June
-      | July
-      | August
-      | September
-      | October
-      | November
-      | December
-    [@@deriving sexp, compare, show]
-
-    let parse month =
-      match month with
-      | "1" -> January
-      | "2" -> February
-      | "3" -> March
-      | "4" -> April
-      | "5" -> May
-      | "6" -> June
-      | "7" -> July
-      | "8" -> August
-      | "9" -> September
-      | "10" -> October
-      | "11" -> November
-      | "12" -> December
-      | _ -> raise (Invalid_argument ("Invalid month value: " ^ month))
-  end
-
-  module DayOfTheWeek = struct
-    type t =
-      | Sunday
-      | Monday
-      | Tuesday
-      | Wednesday
-      | Thursday
-      | Friday
-      | Saturday
-    [@@deriving sexp, compare, show]
-
-    let parse day =
-      match day with
-      | "0" -> Sunday
-      | "1" -> Monday
-      | "2" -> Tuesday
-      | "3" -> Wednesday
-      | "4" -> Thursday
-      | "5" -> Friday
-      | "6" -> Saturday
-      | _ -> raise (Invalid_argument ("Invalid day of the week value: " ^ day))
-  end
+  let truncate_day (time : Time.t) : Time.t =
+    let zone = Lazy.force Zone.local in
+    let date = Time.to_date ~zone time in
+    let date = Date.add_days date 1 in
+    let ofday = Time.to_ofday ~zone time in
+    Time.of_date_ofday ~zone date ofday
 end
 
-(*
-The specification of days can be made by two fields (day of the month and
-day of the week). If month, day of month, and day of week are all asterisks,
-every day shall be matched. If either the month or day of month is specified
-as an element or list, but the day of week is an asterisk, the month and day
-of month fields shall specify the days that match. If both month and day of
-month are specified as an asterisk, but day of week is an element or list,
-then only the specified days of the week match.
+(* The specification of days can be made by two fields (day of the month and
+ * day of the week). If month, day of month, and day of week are all asterisks,
+ * every day shall be matched. If either the month or day of month is specified
+ * as an element or list, but the day of week is an asterisk, the month and day
+ * of month fields shall specify the days that match. If both month and day of
+ * month are specified as an asterisk, but day of week is an element or list,
+ * then only the specified days of the week match.
+ *
+ * Finally, if either the month or day of month is specified as an element or
+ * list, and the day of week is also specified as an element or list, then any
+ * day matching either the month and day of month, or the day of week, shall
+ * be matched. *)
 
-Finally, if either the month or day of month is specified as an element or
-list, and the day of week is also specified as an element or list, then any
-day matching either the month and day of month, or the day of week, shall
-be matched.
-*)
-module Schedule = struct
-  open Time
+(* An element shall be either a number or two numbers separated by
+ * a hyphen (meaning an inclusive range). *)
+type 'time element =
+  | Single of 'time
+  | Range of {
+      max : 'time;
+      min : 'time;
+    }
+[@@deriving sexp, compare]
 
-  (* An element shall be either a number or two numbers separated by
-  a hyphen (meaning an inclusive range). *)
-  type 'time element =
-    | Single of 'time
-    | Range of {
-        max : 'time;
-        min : 'time;
-      }
-  [@@deriving sexp, compare, show]
+(* Each of these patterns can be either an asterisk (meaning
+ * all valid values), an element, or a list of elements separated
+ * by commas. *)
+and 'time value =
+  | All
+  | Value of 'time element
+  | List of 'time element list
+[@@deriving sexp, compare]
 
-  (* Each of these patterns can be either an asterisk (meaning
-  all valid values), an element, or a list of elements separated
-  by commas. *)
-  and 'time value =
-    | All
-    | Value of 'time element
-    | List of 'time element list
-  [@@deriving sexp, compare, show]
+(* In the POSIX locale, the user or application shall ensure that a crontab
+ * entry is a text file consisting of lines of six fields each. The fields
+ * shall be separated by <blank>s. The first five fields shall be integer
+ * patterns that specify the following:
+ * 1. Minute [0,59]
+ * 2. Hour [0,23]
+ * 3. Day of the month [1,31]
+ * 4. Month of the year [1,12]
+ * 5. Day of the week ([0,6] with 0=Sunday) *)
+and schedule = {
+  minute : Span.t value;
+  hour : Span.t value;
+  day_of_month : Span.t value;
+  month : Month.t value;
+  day_of_week : Day_of_week.t value;
+}
+[@@deriving sexp, compare]
 
-  and t = {
-    minute : Minute.t value;
-    hour : Hour.t value;
-    day_of_the_month : DayOfTheMonth.t value;
-    month_of_the_year : MonthOfTheYear.t value;
-    day_of_the_week : DayOfTheWeek.t value;
-  }
-  [@@deriving sexp, compare, show]
+let tokenise value : string * string * string * string * string =
+  let fields =
+    String.split_on_chars value ~on:[ ' '; '\t' ]
+    |> List.filter ~f:(fun field -> not (String.is_empty field))
+    |> List.map ~f:String.strip
+  in
+  match fields with
+  | [ minute_str; hour_str; day_of_month_str; month_str; day_of_week_str ] ->
+      (minute_str, hour_str, day_of_month_str, month_str, day_of_week_str)
+  | _ -> raise (Invalid_argument "Invalid cron expression")
 
-  let parse_element (value : string) (parse : string -> 'time) : 'time element =
-    match value with
-    | value when String.contains value '-' ->
-        String.lsplit2 ~on:'-' value
-        |> Option.map ~f:(fun (min, max) ->
-               Range { max = parse max; min = parse min })
-        |> Option.value_exn
-    | value -> Single (parse value)
+let parse_element (value : string) (parse : int -> 'time) : 'time element =
+  match value with
+  | value when String.contains value '-' ->
+      String.lsplit2 ~on:'-' value
+      |> Option.map ~f:(fun (min, max) ->
+             let min = int_of_string min in
+             let max = int_of_string max in
+             Range { max = parse max; min = parse min })
+      |> Option.value_exn
+  | value ->
+      let value = int_of_string value in
+      Single (parse value)
 
-  let parse_value (value : string) (parse : string -> 'time) : 'time value =
-    match value with
-    | "*" -> All
-    | value when String.contains value ',' ->
-        let elements =
-          String.split value ~on:','
-          |> List.map ~f:(fun value -> parse_element value parse)
-        in
-        List elements
-    | value -> Value (parse_element value parse)
+let parse_value (value : string) (parse : int -> 'time) : 'time value =
+  match value with
+  | "*" -> All
+  | value when String.contains value ',' ->
+      let elements =
+        String.split value ~on:','
+        |> List.map ~f:(fun value -> parse_element value parse)
+      in
+      List elements
+  | value -> Value (parse_element value parse)
 
-  let tokenise value : string * string * string * string * string =
-    let fields =
-      String.split_on_chars value ~on:[ ' '; '\t' ]
-      |> List.filter_map ~f:(fun s ->
-             let stripped = String.strip s in
-             let is_empty = String.is_empty stripped in
-             Option.some_if (not is_empty) stripped)
-    in
-    match fields with
-    | [ minute_str; hour_str; day_str; month_str; dow_str ] ->
-        (minute_str, hour_str, day_str, month_str, dow_str)
-    | _ -> raise (Invalid_argument "Invalid cron expression")
+let parse_schedule (value : string) : schedule =
+  let minute_str, hour_str, day_str, month_str, dow_str = tokenise value in
+  let minute = parse_value minute_str (fun min -> Time.Span.create ~min ()) in
+  let hour = parse_value hour_str (fun hr -> Time.Span.create ~hr ()) in
+  let day = parse_value day_str (fun day -> Time.Span.create ~day ()) in
+  let month = parse_value month_str (fun month -> Month.of_int_exn month) in
+  let day_of_week =
+    parse_value dow_str (fun day -> Day_of_week.of_int_exn day)
+  in
+  { minute; hour; day_of_month = day; month; day_of_week }
 
-  let parse (value : string) : t =
-    let minute_str, hour_str, day_str, month_str, dow_str = tokenise value in
-    let minute = parse_value minute_str Minute.parse in
-    let hour = parse_value hour_str Hour.parse in
-    let day_of_the_month = parse_value day_str DayOfTheMonth.parse in
-    let month_of_the_year = parse_value month_str MonthOfTheYear.parse in
-    let day_of_the_week = parse_value dow_str DayOfTheWeek.parse in
-    { minute; hour; day_of_the_month; month_of_the_year; day_of_the_week }
-end
+(* The specification of days can be made by two fields (day
+ * of the month and day of the week). If month, day of month, and day
+ * of week are all <asterisk> characters, every day shall be matched.
+ * If either the month or day of month is specified as an element or
+ * list, but the day of week is an <asterisk>, the month and day of
+ * month fields shall specify the days that match. If both month and
+ * day of month are specified as an <asterisk>, but day of week is an
+ * element or list, then only the specified days of the week match.
+ * Finally, if either the month or day of month is specified as an
+ * element or list, and the day of week is also specified as an
+ * element or list, then any day matching either the month and day of
+ * month, or the day of week, shall be matched. *)
+
+let element_to_span ~(to_span : int -> Span.t) ~(to_int : Span.t -> int)
+    (element : 'time element) : Span.t list =
+  match element with
+  | Single el -> [ el ]
+  | Range { min; max } ->
+      List.range (to_int min) (to_int max) |> List.map ~f:to_span
+
+let value_to_span ~(min : int) ~(max : int) ~(to_span : int -> Span.t)
+    ~(to_int : Span.t -> int) (value : 'time value) : Span.t list =
+  match value with
+  | All -> List.range min max |> List.map ~f:to_span
+  | Value el -> element_to_span ~to_span ~to_int el
+  | List els -> List.concat_map els ~f:(element_to_span ~to_span ~to_int)
+
+(* Returns next occurrence after UNIX_EPOCH according to the schedule. *)
+let next (schedule : schedule) ~(start_from : Time.t) : Time.t list =
+  let { minute; hour; _ } = schedule in
+  let next_hour = Time.add (U.truncate_hr start_from) (Span.create ~hr:1 ()) in
+  let _minutes =
+    value_to_span ~min:0 ~max:59
+      ~to_span:(fun int -> Span.create ~min:int ())
+      ~to_int:(fun span -> Span.to_min span |> int_of_float)
+      minute
+    |> List.map ~f:(fun minute -> Time.add next_hour minute)
+  in
+  let next_day = Time.add (U.truncate_day start_from) (Span.create ~day:1 ()) in
+  let _hours =
+    value_to_span ~min:0 ~max:23
+      ~to_span:(fun int -> Span.create ~hr:int ())
+      ~to_int:(fun span -> Span.to_hr span |> int_of_float)
+      hour
+    |> List.map ~f:(fun hour -> Time.add next_day hour)
+  in
+  []
--- test/test_ocron.ml
+++ test/test_ocron.ml
@@ -1,14 +1,85 @@
 open Ocron
+open Core
+module Time = Time_float_unix
+module Span = Time.Span
+module Zone = Time.Zone
 
 let%test_unit _ =
-  let actual = Schedule.parse "* * * * *" in
-  let expected : Schedule.t =
+  let actual = parse_schedule "2 * * * *" in
+  let expected : schedule =
     {
-      minute = All;
+      minute = Value (Single (Span.create ~min:2 ()));
       hour = All;
-      day_of_the_month = All;
-      month_of_the_year = All;
-      day_of_the_week = All;
+      day_of_month = All;
+      month = All;
+      day_of_week = All;
     }
   in
-  [%test_eq: Schedule.t] actual expected
+  [%test_eq: schedule] actual expected
+
+let%test_unit _ =
+  let actual = parse_schedule "2-5 * * * *" in
+  let expected : schedule =
+    {
+      minute =
+        Value
+          (Range { min = Span.create ~min:2 (); max = Span.create ~min:5 () });
+      hour = All;
+      day_of_month = All;
+      month = All;
+      day_of_week = All;
+    }
+  in
+  [%test_eq: schedule] actual expected
+
+let%test_unit _ =
+  let actual = parse_schedule "2,3,5 * * * *" in
+  let expected : schedule =
+    {
+      minute =
+        List
+          [
+            Single (Span.create ~min:2 ());
+            Single (Span.create ~min:3 ());
+            Single (Span.create ~min:5 ());
+          ];
+      hour = All;
+      day_of_month = All;
+      month = All;
+      day_of_week = All;
+    }
+  in
+  [%test_eq: schedule] actual expected
+
+let%test_unit _ =
+  let actual = parse_schedule "2,3,5-9 * * 12 *" in
+  let expected : schedule =
+    {
+      minute =
+        List
+          [
+            Single (Span.create ~min:2 ());
+            Single (Span.create ~min:3 ());
+            Range { min = Span.create ~min:5 (); max = Span.create ~min:9 () };
+          ];
+      hour = All;
+      day_of_month = All;
+      month = Value (Single Month.dec);
+      day_of_week = All;
+    }
+  in
+  [%test_eq: schedule] actual expected
+
+let%test_unit _ =
+  let start_from = Time.now () in
+  let schedule = parse_schedule "2 * * * *" in
+  let actual = List.hd_exn (next schedule ~start_from) in
+  let expected =
+    let zone = Lazy.force Zone.local in
+    let date = Time.to_date ~zone start_from in
+    let ofday = Time.to_ofday ~zone start_from in
+    let parts = Time.Ofday.to_parts ofday in
+    let ofday = Time.Ofday.create ~hr:(parts.hr + 1) ~min:2 () in
+    Time.of_date_ofday ~zone date ofday
+  in
+  [%test_eq: Time.t] actual expected
